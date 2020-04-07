20cf712421c79d2b992976e35c35d7304b07fdd5
samartsevaas <anastastasiia.samartceva@gmail.com>
2020-04-07 19:18:13 +0300

    enden tooltip

--- tooltip.js
+++ tooltip.js
@@ -15,7 +15,9 @@ tool.onmouseover = function(event){
     tooltipElem.innerHTML = tooltipHtml;
 
     let elemPosition = tool.getBoundingClientRect();
+    console.log(elemPosition);
     let elemCoord = target.dataset.position;
+    let positionRight = elemPosition.left;
     switch(elemCoord){
         case 'bottom':
     tooltipElem.style.top = elemPosition.top +'px';
@@ -26,13 +28,32 @@ tool.onmouseover = function(event){
     break;
         case 'right':
     tooltipElem.style.top = elemPosition.top + 8 +'px';
-    tooltipElem.style.left = elemPosition.left + 160 +  'px';
+    tooltipElem.style.left = elemPosition.left + 160 + 'px';
     break;
     }
+
+    // const ro = new ResizeObserver(entries => {
+    //     for (let entry of entries){
+    //     const moveTool = entry.contentBoxSize
+    //     ? entry.contentBoxSize.inlineSize
+    //     : entry.contentRect.moveTool; 
+    //     if(elemPosition.left < '755px'){
+    //         console.log(elemPosition.left);
+    //         let elemPositionNew = tool.getBoundingClientRect();
+    //         let TEST = elemPosition.left - elemPositionNew.left;
+    //         tooltipElem.style.left = elemPosition.left + 160 + TEST + 'px';
+    //     } 
+    //     }
+    // })
+    // ro.observe(container);
 }
+
+
 
 tool.onmouseout = function(event){
     if(tooltipElem){
         tooltipElem.remove();
     }
 }
+
+
